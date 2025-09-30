632b63aefcdd62e1dfc927eead4a9d3aa88b5603
Connie Leung <cleung334@gmail.com>
2025-09-30 11:07:37 +0800

    refactor: move the common div from the home component to the card component

--- src/app/ui/card/card.component.ts
+++ src/app/ui/card/card.component.ts
@@ -3,8 +3,10 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-card',
   template: `
-    <div class="bg-gray-800 p-10 rounded-xl shadow-2xl border border-gray-700">
-      <ng-content />
+    <div class="flex flex-col justify-center h-full text-center animate-fade-in">
+      <div class="bg-gray-800 p-10 rounded-xl shadow-2xl border border-gray-700">
+        <ng-content />
+      </div>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,

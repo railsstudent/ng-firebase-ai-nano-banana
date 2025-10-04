8926ecf32b18af1235c14bfc23f6cd1251248201
Connie Leung <cleung334@gmail.com>
2025-10-04 14:30:27 +0800

    feat: add clear image button

--- src/app/ui/image-viewer/image-viewer.component.ts
+++ src/app/ui/image-viewer/image-viewer.component.ts
@@ -1,34 +1,18 @@
 import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
-import { DownloadIconComponent } from '../../editor/icons/download-icon.component';
+import { DownloadIconComponent } from '../icons/download-icon.component';
+import { TrashIconComponent } from '../icons/trash-icon.component';
 import { ImageStyleComponent } from '../image-style/image-style.component';
 
 @Component({
   selector: 'app-image-viewer',
-  imports: [DownloadIconComponent, ImageStyleComponent],
+  imports: [DownloadIconComponent, TrashIconComponent, ImageStyleComponent],
   templateUrl: './image-viewer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageViewerComponent {
   url = input('');
-
-  // Image quality signals
-  // brightness = signal(100);
-  // contrast = signal(100);
-  // saturation = signal(100);
-
-  // imageFilterStyle = computed(() => {
-  //   const normalizedBrightness = this.brightness() / 100;
-  //   const normalizedContrast = this.contrast() / 100;
-  //   const normalizedSaturation = this.saturation() / 100;
-  //   return `brightness(${normalizedBrightness}) contrast(${normalizedContrast}) saturate(${normalizedSaturation})`
-  // });
-
-  downloadImage = output();
   imageFilterStyle = signal<string>('');
 
-  // resetImageControls(): void {
-  //   this.brightness.set(100);
-  //   this.contrast.set(100);
-  //   this.saturation.set(100);
-  // }
+  downloadImage = output();
+  clearImage = output();
 }

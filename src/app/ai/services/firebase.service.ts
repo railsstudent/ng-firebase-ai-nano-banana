b5ebea83ed67e26e8d5f7f8ee9d769b273cb5107
Connie Leung <cleung334@gmail.com>
2025-10-05 18:54:52 +0800

    refactor: firebase service

--- src/app/ai/services/firebase.service.ts
+++ src/app/ai/services/firebase.service.ts
@@ -2,24 +2,37 @@ import { inject, Injectable } from '@angular/core';
 import { InlineDataPart, Part } from 'firebase/ai';
 import { NANO_BANANA_MODEL } from '../constants/firebase.constant';
 
+async function fileToGenerativePart(file: File) {
+  return await new Promise<InlineDataPart>((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve({
+      inlineData: {
+        data: (reader.result! as string).split(',')[1],
+        mimeType: file.type
+      }
+    });
+    reader.readAsDataURL(file);
+  });
+}
+
+async function resolveImageParts(imageFiles: File[]) {
+  if (!imageFiles.length) {
+    return [];
+  }
+
+  const imagePartResults = await Promise.allSettled(
+    imageFiles.map(file => fileToGenerativePart(file))
+  );
+  return imagePartResults
+    .filter((result) => result.status === 'fulfilled')
+    .map((result) => result.value);
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService  {
     private readonly geminiModel = inject(NANO_BANANA_MODEL);
-
-    private async fileToGenerativePart(file: File) {
-      return await new Promise<InlineDataPart>((resolve) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve({
-          inlineData: {
-            data: (reader.result! as string).split(',')[1],
-            mimeType: file.type
-          }
-        });
-        reader.readAsDataURL(file);
-      });
-    }
 
     private async getBase64Image(parts: Array<string | Part>) {
       const result = await this.geminiModel.generateContent(parts);
@@ -31,26 +44,13 @@ export class FirebaseService  {
       throw new Error('Error in generating the image.');
     }
 
-    private async resolveImageParts(imageFiles: File[]) {
-      if (!imageFiles.length) {
-        return [];
-      }
-
-      const imagePartResults = await Promise.allSettled(
-        imageFiles.map(file => this.fileToGenerativePart(file))
-      );
-      return imagePartResults
-        .filter((result) => result.status === 'fulfilled')
-        .map((result) => result.value);
-    }
-
     async generateImage(prompt: string, imageFiles: File[]) {
         try {
           if (!prompt) {
             throw Error('Prompt is required to generate an image.');
           }
 
-          const imageParts = await this.resolveImageParts(imageFiles);
+          const imageParts = await resolveImageParts(imageFiles);
           return await this.getBase64Image([prompt, ...imageParts]);
         } catch (err) {
           console.error('Prompt or candidate was blocked:', err);

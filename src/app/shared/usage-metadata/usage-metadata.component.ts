f73171473952ce022f23b0bc6a696df8f3a5e6cc
Connie Leung <cleung334@gmail.com>
2025-11-17 00:13:53 +0800

    feat: usage-metadata component

---
+++ src/app/shared/usage-metadata/usage-metadata.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usage-metadata',
+  imports: [],
+  templateUrl: './usage-metadata.component.html',
+  styleUrl: './usage-metadata.component.css',
+})
+export class UsageMetadataComponent {
+
+}

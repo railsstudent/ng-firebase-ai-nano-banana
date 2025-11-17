7f1baebec2023ca3c227b80934bdf1d03dde61b0
Connie Leung <cleung334@gmail.com>
2025-11-17 23:55:51 +0800

    feat: style the token component

--- src/app/shared/token-usage/token-usage.component.ts
+++ src/app/shared/token-usage/token-usage.component.ts
@@ -1,11 +1,27 @@
 import { TokenUsage } from '@/ai/types/token-usage.type';
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-token-usage',
   imports: [],
-  templateUrl: './token-usage.component.html',
-  styleUrl: './token-usage.component.css',
+  template: `
+@let tokenStat = tokenUsage();
+@if (tokenStat) {
+  <h3 class="text-indigo-500 text-xl">Token Usage</h3>
+  <div class="w-full">
+    <div class="flex flex-wrap items-center justify-around">
+      <span>Input tokens</span>
+      <span>Output tokens</span>
+      <span>Total tokens</span>
+    </div>
+    <div class="flex flex-wrap items-center justify-around">
+      <span>{{ tokenStat.promptTokenCount }}</span>
+      <span>{{ tokenStat.outputTokenCount }}</span>
+      <span>{{ tokenStat.totalTokenCount }}</span>
+    </div>
+</div>
+}`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TokenUsageComponent {
   tokenUsage = input<TokenUsage| undefined>(undefined);

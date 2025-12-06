3987485ea2a2f849b50591c0f012e95989472a69
Connie Leung <cleung334@gmail.com>
2025-12-06 13:46:14 +0800

    refactor:  Angular is zoneless by default

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideFirebase } from './ai/providers/firebase.provider';
 import { provideGemini } from './ai/providers/gemini.provider';
@@ -7,7 +7,6 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideFirebase(),

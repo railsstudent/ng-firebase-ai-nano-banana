1918d7ebb86d61afba849bc1157a9d2e8c2c1219
Connie Leung <cleung334@gmail.com>
2025-12-16 19:01:17 +0800

    chore: remove the unnecessary casting

--- firebase-project/functions/src/video/index.ts
+++ firebase-project/functions/src/video/index.ts
@@ -1,28 +1,11 @@
 import {onCall} from "firebase-functions/https";
 import {generateVideoFunction} from "./generate-video";
 import {generateVideoFromFramesFunction} from "./interpolate-video-by-frames";
-import {GenerateVideoFromFramesRequest, GenerateVideoRequest} from "../types/video.type";
 
 export const generateVideo = onCall( {cors: true, enforceAppCheck: true},
-  ({data, app}) => {
-    const videoRequest = data as GenerateVideoRequest;
-    const appId = process.env?.APP_ID || "";
-    if (app?.appId && app?.appId === appId) {
-      return generateVideoFunction(videoRequest);
-    }
-
-    throw new Error("App Check failed");
-  }
+  ({data}) => generateVideoFunction(data)
 );
 
 export const interpolateVideo = onCall( {cors: true, enforceAppCheck: true, timeoutSeconds: 180},
-  ({data, app}) => {
-    const videoRequest = data as GenerateVideoFromFramesRequest;
-    const appId = process.env?.APP_ID || "";
-    if (app?.appId && app?.appId === appId) {
-      return generateVideoFromFramesFunction(videoRequest);
-    }
-
-    throw new Error("App Check failed");
-  }
+  ({data}) => generateVideoFromFramesFunction(data)
 );

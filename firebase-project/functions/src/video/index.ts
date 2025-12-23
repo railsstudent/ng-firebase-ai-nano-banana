0bb242c817a5381e616592df492b6c3bf70d5254
Connie Leung <cleung334@gmail.com>
2025-12-23 23:09:57 +0800

    chore: increase the timeout to 3 minutes

--- firebase-project/functions/src/video/index.ts
+++ firebase-project/functions/src/video/index.ts
@@ -6,12 +6,13 @@ const cors = process.env.WHITELIST ? process.env.WHITELIST.split(",") : true;
 const options = {
   cors,
   enforceAppCheck: true,
+  timeoutSeconds: 180,
 };
 
 export const generateVideo = onCall( options,
   ({ data }) => generateVideoFunction(data)
 );
 
-export const interpolateVideo = onCall( { ...options, timeoutSeconds: 180 },
+export const interpolateVideo = onCall( options,
   ({ data }) => generateVideoFromFramesFunction(data)
 );

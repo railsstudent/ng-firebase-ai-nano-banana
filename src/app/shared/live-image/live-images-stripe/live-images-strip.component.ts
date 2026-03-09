17ede9a5309350dfbae1c4f0841e457cfa4024bd
Connie Leung <cleung334@gmail.com>
2026-03-09 21:21:15 +0800

    feat: append thumbnail

--- src/app/shared/live-image/live-images-stripe/live-images-strip.component.ts
+++ src/app/shared/live-image/live-images-stripe/live-images-strip.component.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, linkedSignal, signal } from '@angular/core';
+import { ThumbnailHolder } from './types/live-image-stripe.type';
 
+const CAPACITY = 5;
 @Component({
   selector: 'app-live-images-strip',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   template: `
     <div>
-      live-images-strip working!
+      @for (thumbnail of thumbnailHolders(); track thumbnail.index) {
+        <div (click)="thumbnailSelected(thumbnail.index)">
+          <img [src]="thumbnail.thumbnail" />
+        </div>
+      }
     </div>
   `,
   styles: `
@@ -15,4 +21,33 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     }
   `,
 })
-export class LiveImagesStripComponent {}
+export class LiveImagesStripComponent {
+  newThumbnail = signal<string | null>(null);
+
+  thumbnailHolders = linkedSignal<string | null, ThumbnailHolder[]>({
+    source: () => this.newThumbnail(),
+    computation: (newDataURL, previous) => {
+      if (!newDataURL) {
+        return previous?.value || [];
+      }
+
+      const holder = previous?.value;
+      const index = holder?.length ? holder?.length + 1 : 1
+      const newThumbnailHolder: ThumbnailHolder = {
+        index,
+        thumbnail: newDataURL,
+        original: newDataURL,
+      }
+
+      return !holder ? [newThumbnailHolder] : [...holder, newThumbnailHolder]
+    }
+  });
+
+  generateThumbnail(dataURL: string) {
+    console.log(`Generate thumbnail: ${dataURL}`);
+  }
+
+  thumbnailSelected(index: number) {
+    console.log(`select ${index}`);
+  }
+}
---
+++ src/app/shared/live-image/live-images-stripe/types/live-image-stripe.type.ts
@@ -0,0 +1,5 @@
+export type ThumbnailHolder = {
+  index: number;
+  thumbnail: string;
+  original: string;
+}
